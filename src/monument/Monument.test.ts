cf1b03488aff5738bdbfd403b4712c2b2fb3f058
Ivet Lopez <ivet19@live.com>
2025-04-02 20:06:29 +0200

    Create data layout and test new instances

---
+++ src/monument/Monument.test.ts
@@ -0,0 +1,56 @@
+import Monument from "./Monument.js";
+
+describe("Given the Eiffel Tower", () => {
+  describe("When it is instanciated", () => {
+    test("Then it should indicate that is placed in Paris", () => {
+      const eiffelTower = new Monument("", "", "", {
+        city: "Paris",
+        country: "",
+      });
+      const expectedCity = "Paris";
+
+      const actualCity = eiffelTower.location.city;
+
+      expect(actualCity).toBe(expectedCity);
+    });
+
+    test("Then it should indicate that is placed in France", () => {
+      const eiffelTower = new Monument("", "", "", {
+        city: "",
+        country: "France",
+      });
+      const expectedCountry = "France";
+
+      const actualCountry = eiffelTower.location.country;
+
+      expect(actualCountry).toBe(expectedCountry);
+    });
+
+    test("Then it should be called 'Eiffel Tower'", () => {
+      const eiffelTower = new Monument("Eiffel Tower", "", "", {
+        city: "",
+        country: "",
+      });
+      const expectedName = "Eiffel Tower";
+
+      const actualName = eiffelTower.name;
+
+      expect(actualName).toBe(expectedName);
+    });
+
+    test("Then it should be described as 'A majestic iron lattice tower and global symbol of Parisian elegance and innovation'", () => {
+      const eiffelTower = new Monument(
+        "",
+        "A majestic iron lattice tower and global symbol of Parisian elegance and innovation",
+        "",
+        { city: "", country: "" },
+      );
+      const expectedDescription =
+        "A majestic iron lattice tower and global symbol of Parisian elegance and innovation";
+
+      const actualDescription = eiffelTower.description;
+
+      expect(actualDescription).toBe(expectedDescription);
+    });
+  });
+});
---
+++ src/monument/data/monuments.ts
@@ -0,0 +1,45 @@
+import Monument from "../Monument.js";
+import { MonumentStructure } from "../types.js";
+
+const colosseum = new Monument(
+  "Colosseum of Rome",
+  "The Colosseum of Rome is an amphitheater from the Roman Empire time",
+  "https://es.wikipedia.org/wiki/Coliseo#/media/Archivo:Roman_Colosseum_With_Moon.jpg",
+  { country: "Italy", city: "Rome" },
+);
+
+const tajMahal = new Monument(
+  "Taj Mahal",
+  "A breathtaking white marble mausoleum symbolizing eternal love in Agra, India",
+  "https://es.wikipedia.org/wiki/Taj_Mahal#/media/Archivo:Taj_Mahal_(Edited).jpeg",
+  { country: "India", city: "Agra" },
+);
+
+const statueOfLiberty = new Monument(
+  "Statue of Liberty",
+  "A colossal symbol of freedom and democracy standing proudly in New York Harbor",
+  "https://es.wikipedia.org/wiki/Estatua_de_la_Libertad#/media/Archivo:Statue_of_Liberty_7.jpg",
+  { country: "USA", city: "New York" },
+);
+
+const alhambra = new Monument(
+  "Statue of Liberty",
+  "A mesmerizing Moorish palace and fortress with intricate Islamic art in Granada, Spain",
+  "https://unsplash.com/photos/9ZlZ2C8z5T4",
+  { country: "Spain", city: "Granada" },
+);
+
+const bigBen = new Monument(
+  "Statue of Liberty",
+  "An iconic clock tower marking London's skyline with its timeless chimes",
+  "https://es.wikipedia.org/wiki/Big_Ben#/media/Archivo:Palace_of_Westminster,_London_-_Feb_2007.jpg",
+  { country: "England", city: "London" },
+);
+
+export const monuments: MonumentStructure[] = [
+  colosseum,
+  tajMahal,
+  statueOfLiberty,
+  alhambra,
+  bigBen,
+];
