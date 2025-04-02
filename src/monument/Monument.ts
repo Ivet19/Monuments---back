f6dfb69adff226f992cd662f61b6147db2158fe1
Ivet Lopez <ivet19@live.com>
2025-04-02 20:05:13 +0200

    Create Monument interface and class

---
+++ src/monument/Monument.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+import { MonumentStructure, Location } from "./types.js";
+
+class Monument implements MonumentStructure {
+  public id: string;
+  public country: string;
+  public city: string;
+
+  constructor(
+    public name: string,
+    public description: string,
+    public imageUrl: string,
+    public location: Location,
+  ) {
+    this.country = location.country;
+    this.city = location.city;
+    this.id = uuidv4();
+  }
+}
+
+export default Monument;
