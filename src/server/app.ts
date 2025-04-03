83ad935e9de5a7a269ee30654f34b872c165ea37
Ivet Lopez <ivet19@live.com>
2025-04-03 12:35:59 +0200

    Add health check status endponit

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
-import MonumentController from "../monument/controller/monumentController.js";
+import MonumentController from "../monument/controller/MonumentController.js";
 import { monuments } from "../monument/data/monuments.js";
 
 const app = express();
 
 app.use(morgan("dev"));
+
+app.get("/", (_req, res) => {
+  res.status(200).json({ message: "pong 🏓" });
+});
 
 const monumentController = new MonumentController(monuments);
 
