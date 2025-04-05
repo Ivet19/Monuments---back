2ec0efa925866a00c3b144437c328ce96e24a10b
Ivet Lopez <ivet19@live.com>
2025-04-05 16:45:42 +0200

    Delete setCorsOptions function

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,13 +4,12 @@ import cors from "cors";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
 import { checkHealthStatus } from "./middlewares/checkHealthStatus.js";
 import monumentsRouter from "../monument/router/monumentsRouter.js";
-import setCorsOptions from "./setCorsOptions.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use(cors(setCorsOptions));
+app.use(cors({ origin: true, credentials: true }));
 
 app.get("/", checkHealthStatus);
 
