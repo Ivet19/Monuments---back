8eb33b89707959d4d04d35909137ae6d313da982
Ivet Lopez <ivet19@live.com>
2025-04-05 16:34:40 +0200

    Fix cors options

--- src/server/setCorsOptions.ts
+++ src/server/setCorsOptions.ts
@@ -1,10 +1,7 @@
 import cors from "cors";
 
 const setCorsOptions = () => {
-  const allowedOrigins = ["https://monuments-2025.netlify.app"];
-  const options: cors.CorsOptions = {
-    origin: allowedOrigins,
-  };
+  const options: cors.CorsOptions = { origin: true, credentials: true };
 
   return options;
 };
